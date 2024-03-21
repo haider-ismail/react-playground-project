83418532cbcd46a823d659740040ab51cad7fe53
Haider Ismail <haider.i@icloud.com>
2024-03-21 14:26:45 +0000

    upated Access-Control-Allow-Origin header

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -129,7 +129,7 @@ const movies = [
 app.get('/api/v1/jobs/:keyword', async (req, res) => {
   console.log('req.params', req.params);
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://workable-contracts.netlify.app');
   console.log('custom endpint');
   if (req.params.keyword) {
     const data = await fetchData(req.params.keyword);
