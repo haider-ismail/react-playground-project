d94e1adbbb1397a15ab06785b69e705b31b02ebf
Haider Ismail <haider.i@icloud.com>
2024-03-21 16:44:22 +0000

    using express router

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const express = require('express');
+const { Router } = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
@@ -8,6 +9,7 @@ const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLNon
 require('dotenv').config();
 
 const app = express();
+const router = Router();
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
@@ -33,113 +35,21 @@ const JobType = new GraphQLObjectType({
   }),
 });
 
-// Dummy response
-const movies = [
-  {
-    Title: 'Haider',
-    Year: '2014',
-    imdbID: 'tt3390572',
-    Type: 'movie',
-    Poster: 'https://m.media-amazon.com/images/M/MV5BMjA1NTEwMDMxMF5BMl5BanBnXkFtZTgwODkzMzI0MjE@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Haider lebt - 1. April 2021',
-    Year: '2002',
-    imdbID: 'tt0339098',
-    Type: 'movie',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZTU4NDk1ODgtYjRmZC00OTU4LTk5ZDEtOTgxODUwMzRlNDNhXkEyXkFqcGdeQXVyMTQ0MzMwNQ@@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Catching Haider',
-    Year: '2015',
-    imdbID: 'tt4714240',
-    Type: 'movie',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjg5NjlkZDktOTVlMS00YzEzLWE0MjItMjBhYTM2ZmZlYjBjXkEyXkFqcGdeQXVyNzEwMTEzMDY@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Haider Ali',
-    Year: '1978',
-    imdbID: 'tt0249562',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'Von Haider zu Hader',
-    Year: '2005',
-    imdbID: 'tt2602674',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'Haider',
-    Year: '2016',
-    imdbID: 'tt6262766',
-    Type: 'movie',
-    Poster:
-      'https://ia.media-imdb.com/images/M/MV5BNDU1NjFiOWEtM2YzNy00ZTFiLTlhNjEtZjJkMGM4MjZjNzc5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDI5NzUxODU@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Nishan e Haider: Captain Mohammad Sarwar Shaheed',
-    Year: '1980',
-    imdbID: 'tt9260976',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'Jörg Haider - Der letzte Tag',
-    Year: '2018',
-    imdbID: 'tt11591570',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'Jörg Haider - Politiker, Populist, Popstar',
-    Year: '2009',
-    imdbID: 'tt11698034',
-    Type: 'movie',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZGEyMDYyY2QtNTJjMi00ODZkLTgwOTAtZDM2YjUzNjYxZjA0XkEyXkFqcGdeQXVyMjA2NzE1OQ@@._V1_SX300.jpg',
-  },
-  {
-    Title: 'Haider Khan',
-    Year: '2000',
-    imdbID: 'tt14223648',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'Haider Daler',
-    Year: '1978',
-    imdbID: 'tt14223656',
-    Type: 'movie',
-    Poster: 'N/A',
-  },
-  {
-    Title: 'The Haider Rifaat Show',
-    Year: '2016–2021',
-    imdbID: 'tt15851822',
-    Type: 'series',
-    Poster: 'N/A',
-  },
-];
-
 // Setting up custom endpoint to retrieve movies
-app.get('/api/v1/jobs/:keyword', async (req, res) => {
-  console.log('req.params', req.params);
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  console.log('custom endpint');
-  if (req.params.keyword) {
-    const data = await fetchData(req.params.keyword);
-    // return setTimeout(() => {
-    return res.end(JSON.stringify(data));
-    // }, 5000)
-  }
+// router.get('/api/v1/jobs/:keyword', async (req, res) => {
+//   console.log('req.params', req.params);
+//   res.setHeader('Content-Type', 'application/json');
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   console.log('custom endpint');
+//   if (req.params.keyword) {
+//     const data = await fetchData(req.params.keyword);
+//     // return setTimeout(() => {
+//     return res.end(JSON.stringify(data));
+//     // }, 5000)
+//   }
 
-  res.end(JSON.stringify({ message: 'Error' }));
-});
+//   res.end(JSON.stringify({ message: 'Error' }));
+// });
 
 // Construct a schema, using GraphQL schema language
 const RootQueryType = new GraphQLObjectType({
@@ -174,8 +84,9 @@ var corsOptions = {
   origin: '*',
 };
 
-app.use(
-  'api/graphql',
+// changed from app.use('/api/graphql)
+router.get(
+  '/graphql',
   cors(corsOptions),
   graphqlHTTP({
     schema: schema,
@@ -259,6 +170,8 @@ const fetchData = async (keyword = null) => {
   return data.Search;
 };
 
-app.listen(3001);
+// app.listen(3001);
+
+app.use("/api/", router);
 
 export const handler = serverless(app);
